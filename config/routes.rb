d6b8bcbd2afbf2461e3b8c65fb2171edddfca0ee
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-03-27 15:30:31 +0900

    rails g controller Pages index show create delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/create'
+
+  get 'pages/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
