9b3d7ea2222038a2f218708f80bda84531602a15
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-03-27 16:18:25 +0900

    add controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,26 @@
 class PagesController < ApplicationController
   def index
+    @pages = Page.all
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirecto_to @page
+    else
+      render 'index'
+    end
   end
 
-  def delete
+  def destroy
   end
+
+  private
+    def page_params
+      params.require(:page).permit(:url)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-
-  get 'pages/show'
-
-  get 'pages/create'
-
-  get 'pages/delete'
+  root   'pages#index'
+  resources :pages, only: [:index, :show, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
