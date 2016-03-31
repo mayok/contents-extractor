1e178c921c847ef9f6fbe49bb5e55f1b6468600a
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-03-31 21:20:08 +0900

    use static_pages#home to list pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    # if logged_in then redirect_to pages_path end
+    @pages = current_user.pages.all
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @pages = @user.pages.all
   end
 
   def edit
